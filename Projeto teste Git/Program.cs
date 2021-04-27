d37e9852c8b7154a2dfb2e551e70c57bddf78d10
Gabriel Vieira <gab.cs.1337@outlook.com>
2021-04-27 20:56:09 -0300

    Acrescentei sistema para comprimentar o usuário de acordo com a hora local

--- Projeto teste Git/Program.cs
+++ Projeto teste Git/Program.cs
@@ -7,6 +7,21 @@ namespace Projeto_teste_Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            DateTime t = new DateTime();
+            if (t.Hour >= 05.00 && t.Hour <=11.59 )
+            {
+                Console.WriteLine("Bom Dia!!");
+            }
+            else if(t.Hour >= 12.00 && t.Hour <=17.59 )
+            {
+                Console.WriteLine("Boa Tarde!!");
+            }
+            else
+            {
+                Console.WriteLine("Boa Noite!!");
+            }
+            Console.ReadLine();
         }
     }
 }
